f84448bde8908abb1beb43f498cbe4c7d1384759
mvpcyber <mvpcyberjakarta@gmail.com>
2026-09-07 16:23:40 +0700

    Add Laravel deployment structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#090909">
+        <title>Notaris, PPAT, NPAK & Notaris Pasar Modal</title>
+        @vite('src/main.ts')
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+</html>
